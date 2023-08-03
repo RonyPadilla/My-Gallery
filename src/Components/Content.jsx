5027b4f858c962f1fc1cba897c496f39ca0cb9a9
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-08-03 17:50:04 -0600

    add img

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,10 +1,50 @@
-import "../Style/content.css"
+import "../Style/content.css";
 
 function Content() {
-    return(<>
-          <h1>Hola Todos</h1>
-
-    </>)
+  return (
+    <>
+      <section className="content-img">
+        <div>
+          <img
+            src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo1.png?raw=true"
+            className="photo-1"
+            alt="img-gallery"
+          />
+        </div>
+        <div>
+          <img
+            src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo2.png?raw=true"
+            className="photo-2"
+            alt="img-gallery"
+          />
+        </div>
+        <div>
+          <img
+            src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo3.png?raw=true"
+            className="photo-3"
+            alt="img-gallery"
+          />
+        </div>
+        <div>
+          <img
+            src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo4.png?raw=true"
+            className="photo-4"
+            alt="img-gallery"
+          />
+        </div>
+        <div>
+        <img src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo5.png?raw=true" className="photo-5" alt="img-gallery" />
+        </div>
+        <div>
+          <img
+            src="https://github.com/RonyPadilla/My-Gallery/blob/main/src/Img/photo6.png?raw=true"
+            className="photo-6"
+            alt="img-gallery"
+          />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Content;
